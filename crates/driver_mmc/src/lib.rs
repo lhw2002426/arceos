8df140ed4a9790d465957ccfda3ea4903fb57b40
oslab <1466397747@qq.com>
2023-07-26 17:10:19 +0800

    merge

--- crates/driver_mmc/src/lib.rs
+++ crates/driver_mmc/src/lib.rs
@@ -1,8 +1,8 @@
-//! sd card driver for raspi4, 
-//! if you want to use it, change [dev-dependencies] in modules/axfs/Cargo.toml 
-//! from axdriver = { path = "../axdriver", features = ["block", "ramdisk"] } to axdriver = { path = "../axdriver", features = ["block", "mmc"] }
-//! ans change [features] in  modules/axruntime/Cargo.toml
-//! from "fs = ["alloc", "paging", "axdriver/virtio-blk", "dep:axfs"]" to "fs = ["alloc", "paging", "axdriver/mmc", "dep:axfs"]
+//! sd card driver for raspi4,
+//! if you want to use it, change \[dev-dependencies\] in modules/axfs/Cargo.toml
+//! from axdriver = { path = "../axdriver", features = \["block", "ramdisk"\] } to axdriver = { path = "../axdriver", features = \["block", "mmc"\] }
+//! ans change \[features\] in  modules/axruntime/Cargo.toml
+//! from "fs = \["alloc", "paging", "axdriver/virtio-blk", "dep:axfs"\]" to "fs = \["alloc", "paging", "axdriver/mmc", "dep:axfs"\]
 
 #![no_std]
 #![feature(doc_auto_cfg)]
